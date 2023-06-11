2dbc83f965c0c8f57960f4ca0e6a3292c6c98e25
Ajay Sivan <ajayesivan@gmail.com>
2023-06-12 00:43:16 +0530

    Album art & songs

--- public/script.js
+++ public/script.js
@@ -1,5 +1,34 @@
+var songs = [
+    "a-ha - Take On Me.mp3",
+    "Backstreet Boys - I Want It That Way.mp3",
+    "Coldplay - A Sky Full Of Stars.mp3",
+    "Coldplay - Paradise.mp3",
+    "Katy Perry - Firework.mp3",
+    "Pharrell Williams - Happy.mp3",
+];
+var currentSongIndex = 0;
+
+function getSongPath(name) {
+    return 'songs/' + name;
+}
+
+function changeAlbumArt() {
+    var albumArt = document.getElementById('albumArt');
+    var random = Math.floor(Math.random() * 5) + 1;
+    albumArt.src = "images/album-art" + random + ".png";
+}
+
+changeAlbumArt();
+
+function setSongTitle() {
+    var songTitle = document.getElementById('songTitle');
+    songTitle.innerHTML = songs[currentSongIndex];
+}
+
 var audio = new Audio();
-audio.src = 'songs/' + "Coldplay - Youre A Sky Full Of Stars" + '.mp3';
+audio.src = getSongPath(songs[currentSongIndex]);
+setSongTitle();
+
 
 function updateCurrentTime() {
     var currentTime = Math.floor(audio.currentTime);
@@ -22,9 +51,9 @@ setInterval(updateCurrentTime, 1000);
 function togglePlayIcon() {
     var playIcon = document.getElementById('play-icon');
     if (audio.paused) {
-        playIcon.src="icons/pause.png";
+        playIcon.src = "icons/pause.png";
     } else {
-        playIcon.src="icons/play.png";
+        playIcon.src = "icons/play.png";
     }
 }
 
@@ -50,4 +79,21 @@ function playSong() {
     togglePlayIcon();
     togglePlayOrPause();
     togglebackgroundAnimation();
+}
+
+
+function previousSong() {
+    currentSongIndex--;
+    audio.src = getSongPath(songs[currentSongIndex]);
+    audio.play();
+    changeAlbumArt();
+    setSongTitle();
+}
+
+function nextSong() {
+    currentSongIndex++;
+    audio.src = getSongPath(songs[currentSongIndex]);
+    audio.play();
+    changeAlbumArt();
+    setSongTitle();
 }
